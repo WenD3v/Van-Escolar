45c79e9fe86dc213b6fe3ea7696d4d3a11b0fcfd
wendel <wendel.marques245@gmail.com>
2024-03-24 07:26:12 -0300

    alterado parametros de configuração

--- app/config.py
+++ app/config.py
@@ -1,3 +1,8 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:admin@localhost/van'
+    DB_USER = 'admin'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD')
+    DB_HOST = 'localhost'
+    DB_NAME = 'Van'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://admin:admin@{DB_HOST}/{DB_NAME}'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
